877d79f35dfbce03722810c01a7e4a65ad4166ed
Samet Candan <c._samet@hotmail.com>
2025-01-14 00:17:51 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function () {
-  console.log("function is working");
-};
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+//MIDDLEWARES
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu port ${port} de çalışıyor.`);
+});
