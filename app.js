6ad758223fbd3be87761320998e784f5751f7355
Samet Candan <c._samet@hotmail.com>
2025-01-19 13:29:16 +0300

    form and database

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const path = require("path");
+const { json } = require("stream/consumers");
+
+const Photo = require("./models/Photo");
 
 const app = express();
+
+//connect db
+
+mongoose.connect("mongodb://localhost/pcat-test-db");
 
 //TEMPLATE ENGINE
 
@@ -13,10 +21,14 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 //ROUTES
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", { photos });
 });
 
 app.get("/about", (req, res) => {
@@ -25,6 +37,11 @@ app.get("/about", (req, res) => {
 
 app.get("/add", (req, res) => {
   res.render("add");
+});
+
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
